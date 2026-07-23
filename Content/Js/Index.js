5faf0ce8745c81b1f73e3fa019a8616940338a16
Douglas Guimarães <123005590+douglas-guimaraes-mck@users.noreply.github.com>
2026-07-23 17:40:49 +0100

    Add Lourenço to the facilitators list

--- Content/Js/Index.js
+++ Content/Js/Index.js
@@ -11,6 +11,7 @@ function init() {
         { Name: "Bruno", Role: "dev", Team: "alpha" },
         { Name: "Pavel", Role: "pm", Team: "alpha" },
         { Name: "Luis", Role: "dev", Team: "alpha" },
+        { Name: "Louren&ccedil;o", Role: "dev", Team: "alpha" },
     ];
 
     facilitatorsList.sort(SortByName);
